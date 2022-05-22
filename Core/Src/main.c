6b1579e9f7258df48c3d00a44a3881ec434dc4ed
yangjun <yangjun@nanchao.org>
2022-05-22 17:03:10 +0800

    Add uart3 printf

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -15,10 +15,13 @@
   *
   ******************************************************************************
   */
+#include <stdio.h>
+
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "rtc.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -54,7 +57,13 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+#ifdef __GNUC__
+/* With GCC, small printf (option LD Linker->Libraries->Small printf
+ set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
 /* USER CODE END 0 */
 
 /**
@@ -86,8 +95,9 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_RTC_Init();
+  MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  printf("hello firmware\r\n");
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -151,7 +161,18 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+/**
+ * @brief  Retargets the C library printf function to the USART.
+ * @param  None
+ * @retval None
+ */
+PUTCHAR_PROTOTYPE {
+	/* Place your implementation of fputc here */
+	/* e.g. write a character to the USART1 and Loop until the end of transmission */
+	HAL_UART_Transmit(&huart3, (uint8_t*) &ch, 1, 0xFFFF);
 
+	return ch;
+}
 /* USER CODE END 4 */
 
 /**
